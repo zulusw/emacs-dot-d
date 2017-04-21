9446f8a55f1381407fb9a3565e15d8b1486153d4
Andrea Mucignat <andrea@zulusw.com>
2017-04-21 08:48:22 -0700

    Made cmake-mode loading on OS X only.
    (I don't use it on Linux)

--- init.el
+++ init.el
@@ -3,19 +3,29 @@
 ;; Disable the splash screen (to enable it agin, replace the t with 0)
 (setq inhibit-splash-screen t)
 
-(setq cellar-dir (file-name-as-directory "/usr/local/Cellar"))
-
 (add-to-list 'load-path "~/.emacs.d/lisp")
-(add-to-list 'load-path (concat cellar-dir "cmake/3.7.2_1/share/emacs/site-lisp/cmake"))
+
+;; Load cmake only on OSX. I don't really use it on Linux
+(if (eq system-type 'darwin)
+    (progn (setq cellar-dir (file-name-as-directory "/usr/local/Cellar"))
+           (add-to-list 'load-path (concat cellar-dir "cmake/3.7.2_1/share/emacs/site-lisp/cmake"))
+           (require 'cmake-mode)
+           (setq auto-mode-alist
+                 (append
+                  '(("CMakeLists\\.txt\\'" . cmake-mode))
+                  '(("\\.cmake\\'" . cmake-mode))
+                  auto-mode-alist))))
+
+;; Compile mode settings
+(setq compilation-scroll-output t)
+(setq first-error t)
 
 (require 'highlight-chars)
 (require 'org)
-(require 'cmake-mode)
 
 ;;(require 'timeclock-x)
 ;;(timeclock-modeline-display 1)
 ;;(timeclock-initialize)
-;;(add-hook 'emacs-startup-hook #'timeclock-query-in)
 
 
 (hc-toggle-highlight-trailing-whitespace)
@@ -32,9 +42,3 @@
 (add-to-list 'auto-mode-alist '("\\.org\'" . org-mode))
 ;; The above is the default in recent emacsen
 
-;; CMake setup
-(setq auto-mode-alist
-      (append
-       '(("CMakeLists\\.txt\\'" . cmake-mode))
-       '(("\\.cmake\\'" . cmake-mode))
-       auto-mode-alist))
